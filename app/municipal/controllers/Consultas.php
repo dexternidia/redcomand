ec560ff10f858ef8b69fab361c283317d06f54c5
carlosh1989 <elmorochez22@gmail.com>
2017-12-01 07:43:10 -0800

    agregando la cedula en la tabla de municipales en el modulo de admin

--- app/municipal/controllers/Consultas.php
+++ app/municipal/controllers/Consultas.php
@@ -21,7 +21,30 @@ class Consultas
     {
         extract($_POST);
 
+        if(isset($cedula))
+        {
+            $datos_cne = Cne::where('cedula',$cedula)->first();
 
+            if($datos_cne)
+            {
+                $firmo_contra = FirmaContraMaduro::where('cedula',$cedula)->first();
+                $hogares_asignados = HogaresAsignados::where('cedula',$cedula)->first();
+                $hogares_por_asignar = HogaresPorAsignar::where('cedula',$cedula)->first();
+                $por_pensionar = PorPensionar::where('cedula',$cedula)->first();
+                $ya_pensionados = YaPensionados::where('cedula',$cedula)->first();
+                $profesionales = Profesionales::where('cedula',$cedula)->first();
+                $responsable_municipal = MunicipalResponsable::where('cedula',$cedula)->first();
+
+                //Arr($responsable_municipal);
+                View(compact('firmo_contra','hogares_asignados','hogares_por_asignar','por_pensionar','ya_pensionados','profesionales','cedula','responsable_municipal'));
+            }
+            else
+            {
+                Error('consultas/index','No se encuentra en el Registrado en el CNE');
+            }
+        }
+        else
+        {
             $firmo_contra = FirmaContraMaduro::where('cedula',0)->first();
             $hogares_asignados = HogaresAsignados::where('cedula',0)->first();
             $hogares_por_asignar = HogaresPorAsignar::where('cedula',0)->first();
@@ -31,7 +54,7 @@ class Consultas
             $responsable_municipal = MunicipalResponsable::where('cedula',0)->first();
 
             View(compact('firmo_contra','hogares_asignados','hogares_por_asignar','por_pensionar','ya_pensionados','profesionales','cedula','responsable_municipal'));
-        
+        }
     }
 
     public function create()
