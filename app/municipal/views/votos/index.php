52f2dbad7b82fbc5b25f475788766eb298f8d85b
carlosh1989 <elmorochez22@gmail.com>
2017-12-09 11:55:41 -0800

    cambios forzados

--- app/municipal/views/votos/index.php
+++ app/municipal/views/votos/index.php
@@ -30,7 +30,7 @@ $("#MesasSelect").html(data);
 </script>
 <div id="panel" class="panel panel-primary">
   <div class="panel-heading" style="background-color: red">
-    <h3 class="panel-title text-muted"><i class="fa fa-archive fa-2x"></i> VOTOS<b></b>
+    <h3 class="panel-title text-muted"><i class="fa fa-archive fa-2x"></i> VOTOS CARGA<b></b>
   <a class="btn btn-default pull-right" href="<?php echo baseUrlRole() ?>CuentasUbchMunicipal/create"><i class="fa fa-plus-square text-muted"></i><i style="color:#777;"> ENLAZAR CANDIDATO</i></a
   </h3>
 </div>
