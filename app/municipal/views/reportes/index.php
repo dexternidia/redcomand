419ca12fae4fd07f6750beaef970a7b7418bf5b5
carlosh1989 <elmorochez22@gmail.com>
2017-12-10 09:06:16 -0800

    recuperado lo que borre por locotron en la parte de municipal reportes que es el targhet a la cuestion d resportes de carlos

--- app/municipal/views/reportes/index.php
+++ app/municipal/views/reportes/index.php
@@ -28,55 +28,54 @@ $("#MesasSelect").html(data);
 })
 });
 </script>
-
 <div id="panel" class="panel panel-primary">
   <div class="panel-heading" style="background-color: red">
     <h3 class="panel-title text-muted"><i class="fa fa-file fa-2x"></i> REPORTES<b></b>
-    <a class="btn btn-default pull-right" href="<?php echo baseUrlRole() ?>CuentasUbchMunicipal/create"><i class="fa fa-plus-square text-muted"></i><i style="color:#777;"> CREAR CUENTA</i></a>    </h3>
+    </h3>
   </div>
   <div class="panel-body">
     <div class="col-md-12 table-responsive">
-   <br>
- <form action="http://201.249.75.155/reportes_recomand/index.php" method="GET" target="_blank">
-      <?php echo Token::field() ?>
-      <div class="row">
-        <div class="col-lg-3">
-          <div class="form-group">
-            <select id="" class="form-control" name="id_municipio"/>
-              <?php
-              $user = User();
-              $municipio = \App\MunicipioCne::where('id_municipio',$user['id_municipio'])->first();
-              ?>
-              <option value="<?php echo $municipio->id_municipio ?>">MUNICIPIO <?php echo $municipio->nombre ?></option>
-            </select>
+      <br>
+      <form action="http://201.249.75.155/reportes_recomand/index.php" method="GET" target="_blank">
+        <?php echo Token::field() ?>
+        <div class="row">
+          <div class="col-lg-3">
+            <div class="form-group">
+              <select id="" class="form-control" name="id_municipio"/>
+                <?php
+                $user = User();
+                $municipio = \App\MunicipioCne::where('id_municipio',$user['id_municipio'])->first();
+                ?>
+                <option value="<?php echo $municipio->id_municipio ?>">MUNICIPIO <?php echo $municipio->nombre ?></option>
+              </select>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="form-group">
+              <select id="ParroquiaSelect" class="form-control" name="id_parroquia" required/>
+                <?php
+                $user = User();
+                $parroquias = \App\ParroquiaCne::where('id_municipio',$user['id_municipio'])->get();
+                ?>
+                <option>PARROQUIAS</option>
+                <option></option>
+                <?php foreach ($parroquias as $parroquia): ?>
+                <option value="<?php echo $parroquia->id_parroquia ?>"><?php echo $parroquia->nombre ?></option>
+                <?php endforeach ?>
+              </select>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="form-group">
+              <select id="MesasSelect" class="form-control" name="id_mesa"/>
+              </select>
+            </div>
+          </div>
+          <div class="col-lg-1">
+            <button onclick="enviar()" id="botonSubmit" type="submit" class="btn btn-lg btn-danger pull-right"><i class="fa fa-search fa-1x"></i></button>
           </div>
         </div>
-        <div class="col-lg-3">
-          <div class="form-group">
-            <select id="ParroquiaSelect" class="form-control" name="id_parroquia" required/>
-              <?php
-              $user = User();
-              $parroquias = \App\ParroquiaCne::where('id_municipio',$user['id_municipio'])->get();
-              ?>
-              <option>PARROQUIAS</option>
-              <option></option>
-              <?php foreach ($parroquias as $parroquia): ?>
-              <option value="<?php echo $parroquia->id_parroquia ?>"><?php echo $parroquia->nombre ?></option>
-              <?php endforeach ?>
-            </select>
-          </div>
-        </div>
-        <div class="col-lg-3">
-          <div class="form-group">
-            <select id="MesasSelect" class="form-control" name="id_mesa"/>
-            </select>
-          </div>
-        </div>
-        <div class="col-lg-1">
-          <button onclick="enviar()" id="botonSubmit" type="submit" class="btn btn-lg btn-danger pull-right"><i class="fa fa-search fa-1x"></i></button>
-        </div>
-      </div>
-    </form>
+      </form>
     </div>
   </div>
 </div>
