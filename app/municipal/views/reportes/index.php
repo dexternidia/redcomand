516bc28fb3699e48401a821c7b35b9433c510d3f
carlosh1989 <elmorochez22@gmail.com>
2017-12-09 19:48:41 -0800

    quitando boton en reportes totales

--- app/municipal/views/reportes/index.php
+++ app/municipal/views/reportes/index.php
@@ -32,7 +32,7 @@ $("#MesasSelect").html(data);
 <div id="panel" class="panel panel-primary">
   <div class="panel-heading" style="background-color: red">
     <h3 class="panel-title text-muted"><i class="fa fa-file fa-2x"></i> REPORTES<b></b>
-    <a class="btn btn-default pull-right" href="<?php echo baseUrlRole() ?>CuentasUbchMunicipal/create"><i class="fa fa-plus-square text-muted"></i><i style="color:#777;"> CREAR CUENTA</i></a>    </h3>
+    </h3>
   </div>
   <div class="panel-body">
     <div class="col-md-12 table-responsive">
